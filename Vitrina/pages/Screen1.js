e6d6cec4897009968be38d5abc22222776ce9c7f
MOIRL <106426920+MOIRL@users.noreply.github.com>
2023-08-07 12:36:16 +0200

    Screen1.js

---
+++ Vitrina/pages/Screen1.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Text, TextInput, View, StyleSheet, Image, TouchableOpacity, Button, ScrollView } from 'react-native';
+import Constants from 'expo-constants';
+import { Ionicons } from '@expo/vector-icons';
+
+////////USESTATE HERE
+
+export default function Screen1() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>HOME</Text>
+    </View>
+  );
+}
