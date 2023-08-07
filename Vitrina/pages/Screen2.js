20b924ed12586e6c146f58ea134f02b693f727a3
MOIRL <106426920+MOIRL@users.noreply.github.com>
2023-08-07 12:36:56 +0200

    Screen2.js

---
+++ Vitrina/pages/Screen2.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Text, TextInput, View, StyleSheet, Image, TouchableOpacity, Button, ScrollView } from 'react-native';
+import Constants from 'expo-constants';
+
+
+
+export default function Screen2() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>FAVOURITES</Text>
+    </View>
+  );
+}
